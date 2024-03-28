ac24d621aa3f904fdd47a7147b9870c81aeab8eb
MassimoAzzini <massimo.azzini.web@gmail.com>
2024-03-28 15:33:34 +0100

    [add] Snack4

---
+++ src/org/learning/snack/Snack4.java
@@ -0,0 +1,29 @@
+package org.learning.snack;
+
+import java.util.Scanner;
+
+public class Snack4 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Scrivi una parola e ti dirò se è palindroma: ");
+        String userWord = scan.nextLine().toLowerCase();
+
+        char [] charArray = userWord.toCharArray();
+
+        String reversWord = "";
+
+        for (int i = charArray.length-1; i >= 0 ; i--) {
+
+            reversWord = reversWord + charArray[i];
+        }
+
+        if (reversWord.equals(userWord)){
+            System.out.println("Parola palindroma");
+        } else {
+            System.out.println("Parola non palindroma");
+        }
+
+    }
+}
